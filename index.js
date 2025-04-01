70d7aaad57fe0ea216cf227aaea9b2f3e923a84a
Supratim Debnath <debnathsupratim70@gmail.com>
2025-04-01 19:49:50 +0530

    index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello SDN')
+  res.send('Hello, I am Supratim from MCKVIE')
 })
 
 app.get('/courses', function (req, res) {
